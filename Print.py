6100f090af286615420b9d389326cbbe4f090547
Nathan Reyes <49377262+zeezar4@users.noreply.github.com>
2023-01-15 21:43:30 -0600

    Create Print.py

---
+++ Print.py
@@ -0,0 +1,44 @@
+# so if you are new to Python this is the best place to start understanding commands
+# <-- this symbol right here is used to make comments that wont interfere with the code
+"""
+^ while this tripple quotes 
+allow for 
+a 
+multi
+line 
+comment
+and are closed using a tripple quote as well
+"""
+
+print("now lets get to printing!")
+
+# notice how we use the word print to call out that we want something to be 
+# read on the output, we also have the () parenthesis and " " quites to house the text
+# Remove the comment mark on next line
+
+#print(notice how this will show an error since its not in quotes?)
+
+print("numbers can be printed without quotes" , 2+2, 3, 5) 
+
+# commas can be used to give spaces in python3 unlike previous versions
+#you can also do basic math functions in python lets take a look
+
+print(3+3) # add
+print(3-3) # subtract
+print(3*3) # multiply
+print(3**3) # exponential/"power of"
+print (3/3) # divide
+
+# there are like 3 others feel free to look them up
+
+# lastly in this section we will cover variables
+# variables basically are assigning a set of information to a shorthand version
+# sort of like the X in algebra that everyone is trying to find 
+
+Variable = "this is a variable"
+
+tarter_sauce = "this is also a variable"
+
+Finally = "these all can be printed out in a whole line"
+
+print(Variable , tarter_sauce , Finally)
